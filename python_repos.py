8cc8066a4a52e3a5cc9070f9345289838245daf9
ywfree <yaowan19432003@163.com>
2019-03-27 10:38:51 +0800

    modified

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 #operate api 
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -13,11 +15,9 @@ response_dict = r.json()
 
 #print the value of  key-total_cout
 print("Total repositories:", response_dict['total_count'])
-
-#items is a key of dictionary list
+#items is a key of dictionary list 
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
-
+print("Repositories returned:", len(repo_dicts)) 
 #print first element of the list repo_dicts 
 #repo_dict0 = repo_dicts[0]
 #print("\nThe first element of the list items: ",repo_dict0)
@@ -29,13 +29,40 @@ print("Repositories returned:", len(repo_dicts))
 #for key in sorted(repo_dict1.keys()):
  #   print(key)
 
-print("\nSelected information about first repository:")
+#print("\nSelected information about first repository:")
+#for repo_dict in repo_dicts:
+#   print("\nName:", repo_dict['name'])
+#   print("Owner:", repo_dict['owner']['login'])
+#   print("Stars:", repo_dict['stargazers_count'])
+#   print("Repository:", repo_dict['html_url'])
+#   print("Created:", repo_dict['created_at'])
+#   print("Updated:", repo_dict['updated_at'])
+#   print("Description:", repo_dict['description'])
+names = []
+plot_dicts = []
 for repo_dict in repo_dicts:
-   print("\nName:", repo_dict['name'])
-   print("Owner:", repo_dict['owner']['login'])
-   print("Stars:", repo_dict['stargazers_count'])
-   print("Repository:", repo_dict['html_url'])
-   print("Created:", repo_dict['created_at'])
-   print("Updated:", repo_dict['updated_at'])
-   print("Description:", repo_dict['description'])
+    names.append(repo_dict['name'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': str(repo_dict['description']),
+        'xlink': repo_dict['html_url'],
+        }
+    plot_dicts.append(plot_dict)
 
+#visual
+my_style = LS('#333366', base_style=LCS)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config,style=my_style)
+chart.title = 'Most-Starred Python Projects on GitHub' 
+chart.x_labels = names 
+chart.add('', plot_dicts) 
+chart.render_to_file('python_repos4.svg') 
